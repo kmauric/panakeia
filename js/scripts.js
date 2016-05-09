ea00c919326c3980e5772795827d572b8ff8a1bc
Kristine Mauric <kristine.mauric@gmail.com>
2016-05-09 15:03:53 -0400

    added comments on sass and index page, added jQuery functionality to the burger navigation

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,24 @@ $(function() {
 	
 	$("#main-nav li").addClass("animated slideInRight");
 	
-	
 	// Hamburger Menu
-	
 	$(".toggle-nav").on("click", function(e) {
 		e.preventDefault();
 		$(this).toggleClass("active");
 		$("#main-nav li").removeClass("animated slideInRight");
 		$("#main-nav ul").toggleClass("mobile-nav");
-	});
+		
+	}); //end .toggle-nav click
+	
+	$(document).on("click", function(e) {
+		e.stopPropagation();
+		var $mainNav = $("#main-nav");
+		
+		if ($mainNav.has(e.target).length === 0) {
+			$(".toggle-nav").removeClass("active");
+			$("#main-nav ul").removeClass("mobile-nav");
+		}
+		
+	}); //end document.click
 	
 });
